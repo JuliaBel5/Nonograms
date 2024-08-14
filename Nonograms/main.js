4e79b6fa52357700cd7ab4715641cf78854f7fbf
Julia <119964764+JuliaBel5@users.noreply.github.com>
2024-08-15 01:27:25 +0300

    fix: fix import name

--- Nonograms/main.js
+++ Nonograms/main.js
@@ -11,7 +11,7 @@ import {
 import { createElement } from './utils/createElement'
 import { createGrid } from './utils/createGrid'
 import { View } from './view/View'
-import { Controller } from './controller/Controller'
+import { Controller } from './controller/controller'
 import { Model } from './model/Model'
 
 const theme = JSON.parse(localStorage.getItem('julTheme')) ?? 0
